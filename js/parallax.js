87d78e80d8816b2da9fca5204f49d88d84fca0a3
Soocy <schoell93@gmail.com>
2017-01-06 14:29:32 +0100

    parallax work when i see

--- js/parallax.js
+++ js/parallax.js
@@ -1,13 +1,26 @@
 $(document).ready(function () {
-    $(window).scroll(function (e) {
-        parallax();
+
+
+    $(window).scroll(function () {
+        if(whereIam() < 1011){
+            parallax();
+        }
+        // console.log(whereIam() + 'px');
+        // parallax();
     });
+
+
+    function whereIam() {
+        // var scrollLeft = $(window).scrollLeft();
+        var scrollTop = $(window).scrollTop();
+        return scrollTop;
+    }
+
     function parallax() {
         var scrolled = $(window).scrollTop();
         // $('#layer3').css('top', -(scrolled * 0.2) + 'px'); ez a leghozzám közelebbi ez nem mozog
         $('#layer2').css('margin-top', (scrolled * 0.4) + 'px');
-        $('#layer1').css('margin-top', (scrolled * 0.6) + 'px');
         $('#moon').css('margin-top', (scrolled * 0.1) + 'px');
-
+        $('#layer1').css('margin-top', (scrolled * 0.6) + 'px');
     }
 });
