7ec486b55e064bd381c386ee8fb057293909cb81
Soocy <schoell93@gmail.com>
2017-01-06 12:34:10 +0100

    i hope this is the old version

--- js/parallax.js
+++ js/parallax.js
@@ -8,5 +8,6 @@ $(document).ready(function () {
         $('#layer2').css('margin-top', (scrolled * 0.4) + 'px');
         $('#layer1').css('margin-top', (scrolled * 0.6) + 'px');
         $('#moon').css('margin-top', (scrolled * 0.1) + 'px');
+
     }
 });
