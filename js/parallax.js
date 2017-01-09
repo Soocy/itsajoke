cdc9347d5d14599fb5d318e89fd5c213c2dcca26
Soocy <schoell93@gmail.com>
2017-01-09 08:43:02 +0100

    just nothing

--- js/parallax.js
+++ js/parallax.js
@@ -13,6 +13,7 @@ $(document).ready(function () {
     function whereIam() {
         // var scrollLeft = $(window).scrollLeft();
         var scrollTop = $(window).scrollTop();
+        console.log(scrollTop);
         return scrollTop;
     }
 
@@ -22,5 +23,6 @@ $(document).ready(function () {
         $('#layer2').css('margin-top', (scrolled * 0.4) + 'px');
         $('#moon').css('margin-top', (scrolled * 0.1) + 'px');
         $('#layer1').css('margin-top', (scrolled * 0.6) + 'px');
+        console.log("megy");
     }
 });
